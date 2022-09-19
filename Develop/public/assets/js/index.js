623970213a034060f1982df6a67406cf48a110d6
zsheperdigian <zsheperdigian@gmail.com>
2022-09-18 20:40:07 -0700

    tried fixing the get post

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -5,8 +5,7 @@ let newNoteBtn;
 let noteList;
 
 
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -29,7 +28,7 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes', {
+  fetch('  ', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path'); 
-const db = require('./db/db.json');
 const fs = require('fs');
-
-//variable port to run server
-const PORT = process.env.PORT || 3001;
+const db = require('./db/db.json');
+const uuid = require('./helpers/uuid'); //unique id middleware
+const PORT = process.env.PORT || 3001; //variable port to run server
 // creating variable for express to use
 const app = express();
 
 
 //midleware for parsing JSON data
 console.log('loading middleware');
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
@@ -22,22 +21,59 @@ app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//Route to Get notes pages
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 // Get request for api/notes
 app.get('/api/notes', (req,res) =>{
     res.json({db});
     console.log(db);
+    //fs read and parse
+    const readData = JSON.parse(fs.readFile('./db/db.json', (err) => {
+        err ? console.error(err) : console.log("Success writing notes to db")
+    }));
+    console.log(readData);
 });
 
+// post wrote to write data to db
+app.post('/api/notes', (req,res) =>{
+    // deconstructing the Notetitle and notetext from request and adding to variable
+    const { noteTitle, noteText } = req.body
+    const savedNotes = {
+        noteTitle,
+        noteText,
+    }
 
-//Route to Get notes pages
-app.get('/notes', (req, res) =>
-  console.log('/notes .get');
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+    
+    // variable to hold parsed data of notes
+    const noteData = JSON.parse(fs.readFile('./db/db.json', (err) => {
+        err ? console.error(err) : console.log("Success writing notes to db")
+    }));
+    noteData.push(savedNotes);
+    //debugging
+    console.log(savedNotes);
+    console.log(noteData);
+
+    stringyNote = JSON.stringify(noteData)
+    //writing data with new added note    
+    fs.writeFile('./db/db.json', stringyNote, (err) => {
+    err ? console.error(err) : console.log("Success writing notes to db")
+    });
+
+    //send savedNOtes
+    res.json(savedNotes);
+
+})
+
+
+// route to delete notes when clicked on trashcan
+app.delete('/api/notes/', (req, res) =>{
 
 
 
-
+})
 
 
 
